be8a1960d709566f5609aac6b0c4d1d283983381
Nikolas Miksiewicz <freshnikolas@hotmail.de>
2023-07-30 21:34:02 +0300

    Added Navbuttons, fixed issues, Modified visuals

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,6 @@ module.exports = {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        'lila': {
-          hell: '#EACEF3',
-          DEFAULT: '#D6A1E7',
-          dunkel: '#692580',
-        },
         'blau': {
           stark_hell: '#5CB6FF',
           hell: '#0A91FF',
@@ -21,24 +16,23 @@ module.exports = {
           dunkel: '#004F8F',
           stark_dunkel: '#002D52',
         },
-        'gelb': {
-          hell: '#F8E8C9',
-          DEFAULT: '#F2D398',
-          dunkel: '#E4A125',
-        },
-        'rot': {
-          hell: '#F0BCBF',
-          DEFAULT: '#E0777D',
-          dunkel: '#D2373F',
-          },
         'hinten': {
-          hell: '#fff8f8',
+          hell: '#ededee',
+          dunkel_hover: '#202635',
           leicht_dunkel: '#212A45',
           dunkel: '#151B2B',
           stark_dunkel: '#0B0F17',
         },
+        'text': {
+          hell: '#151B2B',
+          dunkel: '#ededee',
+        },
+        'karte': {
+          hell: '',
+          dunkel: '#252E49',
+        },
       },
     },
-  },
   darkMode: 'class',
+  }
 }
